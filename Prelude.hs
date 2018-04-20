6949357674024df2fdf19231517a08ad7c4ccfee
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-04-19 20:57:21 -0500

    Add simple semaphore benchmark, some additions to Prelude.hs

--- Prelude.hs
+++ Prelude.hs
@@ -1,4 +1,6 @@
-{-# language PackageImports #-}
+{-# language CPP                #-}
+{-# language ExplicitNamespaces #-}
+{-# language PackageImports     #-}
 
 module Prelude
   ( -- * Alternative
@@ -21,13 +23,240 @@ module Prelude
   , ByteString
     -- * Category
   , module Category
+  , Foldable(..)
+  , foldrM
+  , foldlM
+  , traverse_
+  , for_
+  , sequenceA_
+  , asum
+  , mapM_
+  , forM_
+  , sequence_
+  , msum
+  , concat
+  , concatMap
+  , and
+  , or
+  , all
+  , notElem
+  , find
+  , foldM
+  , foldM_
+  , foldMapBy
+  , foldBy
     -- * Foldl
   , module Foldl
     -- * Function
-  , module Function
+  , const
+  , flip
+  , ($)
+  , ($!)
+  , (&)
+  , until
+  , fix
+  , Endo(..)
+    -- * Functor
+  , void
+    -- * IO
+  , IO
+    -- * List
+  , (++)
+  , (!!)
+  , (\\)
+  , allSame
+  , anySame
+  , break
+  , breakOn
+  , breakOnEnd
+  , breakEnd
+  , chunksOf
+  , cons
+  , cycle
+  , delete
+  , deleteBy
+  , deleteFirstsBy
+  , disjoint
+  , drop
+  , dropEnd
+  , dropPrefix
+  , dropSuffix
+  , dropWhile
+  , dropWhileEnd
+  , elemIndex
+  , elemIndices
+  , filter
+  , findIndex
+  , findIndices
+  , genericDrop
+  , genericIndex
+  , genericLength
+  , genericReplicate
+  , genericSplitAt
+  , genericTake
+  , group
+  , groupBy
+  , groupOn
+  , groupSort
+  , groupSortBy
+  , groupSortOn
+  , inits
+  , insert
+  , insertBy
+  , intercalate
+  , intersect
+  , intersectBy
+  , intersperse
+  , isInfixOf
+  , isPrefixOf
+  , isSubsequenceOf
+  , isSuffixOf
+  , iterate
+#if MIN_VERSION_base(4,11,0)
+  , iterate'
+#endif
+  , lines
+  , lookup
+  , map
+  , nub
+  , nubBy
+  , nubOn
+  , nubOrd
+  , nubOrdBy
+  , nubOrdOn
+  , nubSort
+  , nubSortBy
+  , nubSortOn
+  , partition
+  , permutations
+  , repeat
+  , replicate
+  , reverse
+  , scanl
+  , scanl'
+  , scanl1
+  , scanr
+  , scanr1
+  , sort
+  , sortBy
+  , sortOn
+  , span
+  , spanEnd
+  , split
+  , splitAt
+  , splitAtEnd
+  , snoc
+  , stripInfix
+  , stripInfixEnd
+  , stripPrefix
+  , stripSuffix
+  , subsequences
+  , tails
+  , take
+  , takeEnd
+  , takeWhile
+  , takeWhileEnd
+  , transpose
+  , uncons
+  , unfoldr
+  , union
+  , unionBy
+  , unlines
+  , unsnoc
+  , unwords
+  , unzip
+  , unzip3
+  , unzip4
+  , unzip5
+  , unzip6
+  , unzip7
+  , words
+  , zip
+  , zip3
+  , zip4
+  , zip5
+  , zip6
+  , zip7
+  , zipWith
+  , zipWith3
+  , zipWith4
+  , zipWith5
+  , zipWith6
+  , zipWith7
     -- * Monad
   , module Monad
-  , module X
+    -- * Number
+  , Int
+  , Int8
+  , Int16
+  , Int32
+  , Int64
+  , Integer
+  , Natural
+  , Ratio
+  , Rational
+  , numerator
+  , denominator
+  , Word
+  , Word8
+  , Word16
+  , Word32
+  , Word64
+  , Float
+  , Double
+  , Complex
+    -- * Numeric type classes
+  , Num(..)
+  , subtract
+  , fromIntegral
+  , Real(..)
+  , realToFrac
+  , div'
+  , mod'
+  , divMod'
+  , Integral(..)
+  , even
+  , odd
+  , gcd
+  , lcm
+  , Fractional(..)
+  , Floating(..)
+  , RealFrac(..)
+  , RealFloat(..)
+  , fromRat
+  , Erf(..)
+  , InvErf(..)
+  , Nat
+  , KnownNat
+  , natVal
+  , natVal'
+  , SomeNat(..)
+  , someNatVal
+  , sameNat
+  , type (<=)
+  , type (<=?)
+  , type (+)
+  , type (*)
+  , type (^)
+  , type (-)
+  , Min
+  , Max
+  , Lcm
+  , Gcd
+  , Divides
+#if MIN_VERSION_base(4,11,0)
+  , Div
+  , Mod
+  , Log2
+#endif
+  , CmpNat
+    -- * Show
+  , Show(..)
+    -- * Tuple
+  , fst
+  , snd
+  , curry
+  , uncurry
   ) where
 
 import Alternative
@@ -35,7 +264,12 @@ import Applicative
 import ByteString (ByteString)
 import Category
 import Foldl (Fold(..), FoldM(..))
-import Function ((&))
+import Foldable
+import Function
+import Functor
+import IO
+import List
 import Monad
-import "base" Prelude as X hiding
-  ((.), fail, foldl, foldl1, foldr1, head, id, init, last, lex, maximum, minimum, read, return, tail)
+import Number
+import Show
+import Tuple
---
+++ bench/Semaphore.hs
@@ -0,0 +1,71 @@
+{-# language ScopedTypeVariables #-}
+
+import Benchmark
+import Concurrency
+import Eval
+import MVar
+import QSem
+import STM
+import TSem
+
+main :: IO ()
+main =
+  defaultMain
+    [ bgroup "Semaphores"
+        [ bgroup "QSem"
+            [ benchmarkQSem 1
+            , benchmarkQSem 2
+            , benchmarkQSem 4
+            , benchmarkQSem 8
+            ]
+        , bgroup "TSem"
+            [ benchmarkTSem 1
+            , benchmarkTSem 2
+            , benchmarkTSem 4
+            , benchmarkTSem 8
+            ]
+        ]
+    ]
+
+benchmarkQSem :: Int -> Benchmark
+benchmarkQSem n =
+  benchmarkSem n (newQSem 1) waitQSem signalQSem
+
+benchmarkTSem :: Int -> Benchmark
+benchmarkTSem n =
+  benchmarkSem n (atomically (newTSem 1)) (atomically . waitTSem)
+    (atomically . signalTSem)
+
+benchmarkSem :: Int -> IO a -> (a -> IO ()) -> (a -> IO ()) -> Benchmark
+benchmarkSem n mk acquire release =
+  envWithCleanup
+    (setup (n-1) mk action)
+    teardown
+    (\ ~(Setup sem _) ->
+      bench (show n ++ " thread(s)") (whnfIO (action sem)))
+ where
+  action sem = do
+    acquire sem
+    release sem
+
+data Setup a
+  = Setup a (IO ())
+
+instance NFData (Setup a) where
+  rnf _ = ()
+
+setup :: Int -> IO a -> (a -> IO ()) -> IO (Setup a)
+setup n mk action = do
+  sem <- mk
+  ready :: MVar () <-
+    newEmptyMVar
+  ids :: [ThreadId] <-
+    replicateM n . forkIO $ do
+      putMVar ready ()
+      forever (action sem)
+  replicateM_ n (takeMVar ready)
+  pure (Setup sem (traverse_ killThread ids))
+
+teardown :: Setup a -> IO ()
+teardown (Setup _ action) =
+  action
