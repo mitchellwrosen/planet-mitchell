051cc4ca3ab74d030539f2fd43efe54c43947c9f
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-05-08 00:07:18 -0400

    Add Bifoldable.Partial

--- Bifoldable.hs
+++ Bifoldable.hs
@@ -1,21 +1,7 @@
 {-| This module contains:
 
-    * The 'Bifoldable' type class and related functionality from @base@.
-
-    The following functions are not re-exported:
-
-    * 'biconcat' (use 'biasum' instead)
-    * 'bifoldl' (use 'bifoldl'' instead)
-    * 'bifoldl1' (partial)
-    * 'bifoldr1' (partial)
-    * 'biforM_' (use 'bifor_' instead)
-    * 'bimapM'_ (use 'bitraverse_' instead)
-    * 'bimaximum' (partial)
-    * 'bimaximumBy' (partial)
-    * 'biminimum' (partial)
-    * 'biminimumBy' (partial)
-    * 'bimsum' (use 'biasum' instead)
-    * 'bisequenceA_' (use 'bisequence_' instead)
+    * The 'Bifoldable' type class and related functionality from @base@,
+      originally exported from "Data.Bifoldable".
 -}
 
 module Bifoldable
---
+++ Bifoldable/Partial.hs
@@ -0,0 +1,15 @@
+{-| This module contains:
+
+    * The partial functions exported by "Data.Bifoldable" from @base@.
+-}
+
+module Bifoldable.Partial
+  ( bifoldl1
+  , bifoldr1
+  , bimaximum
+  , bimaximumBy
+  , biminimum
+  , biminimumBy
+  ) where
+
+import Data.Bifoldable
