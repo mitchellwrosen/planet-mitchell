95021a24e0608ea185ed3acd8f5558e423e95cf6
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-05-08 00:50:08 -0400

    Add flag(bits)

--- Bits.hs
+++ Bits.hs
@@ -5,14 +5,20 @@
     * The 'srl' and 'msb' functions from @bits@.
 -}
 
+{-# language CPP #-}
+
 module Bits
   ( -- * Bits
     Bits(..)
+#ifdef BITS
   , srl
+#endif
   , toIntegralSized
   , FiniteBits(..)
+#ifdef BITS
   , Ranked(..)
   , msb
+#endif
     -- * Default @Bits@ implementations
   , bitDefault
   , testBitDefault
@@ -20,4 +26,6 @@ module Bits
   ) where
 
 import Data.Bits
+#ifdef BITS
 import Data.Bits.Extras
+#endif
