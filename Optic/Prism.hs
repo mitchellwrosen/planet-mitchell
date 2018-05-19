2cbb947a826d23c02ecf60cb28b9853754c00e8a
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-05-19 14:55:04 -0500

    Add Optic.Prism

---
+++ Optic/Prism.hs
@@ -0,0 +1,14 @@
+module Optic.Prism
+  ( Prism,
+    Prism',
+    APrism,
+    APrism',
+    prism,
+    prism',
+    _Left,
+    _Right,
+    _Just,
+    _Nothing,
+  ) where
+
+import Control.Lens.Prism
