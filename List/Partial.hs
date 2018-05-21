f82429b19aa63aff25137051fb811d68ed3c623a
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-05-21 15:50:30 -0400

    chunksOf/splitOn moved to List.Partial

--- List/Partial.hs
+++ List/Partial.hs
@@ -1,9 +1,20 @@
+{-# language CPP #-}
+
 module List.Partial
-  ( (!!)
-  , head
-  , init
-  , last
-  , tail
+  ( (!!),
+#ifdef USE_EXTRA
+    chunksOf,
+#endif
+    head,
+    init,
+    last,
+#ifdef USE_EXTRA
+    splitOn,
+#endif
+    tail,
   ) where
 
 import Data.List
+#ifdef USE_EXTRA
+import Data.List.Extra
+#endif
