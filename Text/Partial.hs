2590a617425dba424dbd27f4460261f6ceaa049e
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-06-06 00:20:35 -0400

    Split out Text.Partial module

---
+++ Text/Partial.hs
@@ -0,0 +1,22 @@
+module Text.Partial
+  ( count,
+    decodeLatin1,
+    decodeUtf16BE,
+    decodeUtf16LE,
+    decodeUtf32BE,
+    decodeUtf32LE,
+    decodeUtf8,
+    foldl1',
+    foldr1,
+    head,
+    index,
+    init,
+    last,
+    maximum,
+    minimum,
+    splitOn,
+    tail,
+  ) where
+
+import Data.Text
+import Data.Text.Encoding
