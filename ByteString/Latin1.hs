efd317e856182a6eea8cbc56dbddead47027b806
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-06-09 17:07:34 -0500

    Add bytestring-lexing

--- ByteString/Latin1.hs
+++ ByteString/Latin1.hs
@@ -1,5 +1,16 @@
+{-# language CPP #-}
+
 module ByteString.Latin1
-  ( module Data.ByteString.Char8
+  ( module Data.ByteString.Char8,
+#ifdef DEP_BYTESTRING_LEXING
+    readSigned,
+    readDecimal,
+    readHexadecimal,
+    readOctal,
+#endif
   ) where
 
 import Data.ByteString.Char8
+#ifdef DEP_BYTESTRING_LEXING
+import Data.ByteString.Lex.Integral
+#endif
