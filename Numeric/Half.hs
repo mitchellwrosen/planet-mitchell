8fad82611dc09c957c4d5e5ed889592e32a4d720
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-06-13 22:21:57 -0400

    Add half

---
+++ Numeric/Half.hs
@@ -0,0 +1,22 @@
+{-# language PackageImports  #-}
+{-# language PatternSynonyms #-}
+
+module Numeric.Half
+  ( Half(..),
+    isZero,
+    fromHalf,
+    toHalf,
+    pattern POS_INF,
+    pattern NEG_INF,
+    pattern QNaN,
+    pattern SNaN,
+    pattern HALF_MIN,
+    pattern HALF_NRM_MIN,
+    pattern HALF_MAX,
+    pattern HALF_EPSILON,
+    pattern HALF_DIG,
+    pattern HALF_MIN_10_EXP,
+    pattern HALF_MAX_10_EXP,
+  ) where
+
+import "half" Numeric.Half
