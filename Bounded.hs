1291f0ee04165704498453210657695ff71adec4
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-06-15 15:24:43 -0400

    Add semilattices

--- Bounded.hs
+++ Bounded.hs
@@ -1,5 +1,17 @@
+{-# language CPP #-}
+
 module Bounded
-  ( Bounded(..)
+  ( Bounded(..),
+#ifdef DEP_SEMILATTICES
+    Lower(..),
+    Upper(..),
+    Bound(..),
+#endif
   ) where
 
+#ifdef DEP_SEMILATTICES
+import Data.Semilattice.Bound
+import Data.Semilattice.Lower
+import Data.Semilattice.Upper
+#endif
 import GHC.Enum
---
+++ Semilattice.hs
@@ -0,0 +1,9 @@
+module Semilattice
+  ( Join(..),
+    Meet(..),
+    Order(..),
+  ) where
+
+import Data.Semilattice.Join
+import Data.Semilattice.Meet
+import Data.Semilattice.Order
