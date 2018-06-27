29296f98c818cd488e7680552b04a2f454dfcc49
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-06-26 20:05:48 -0500

    Add double-conversion

--- Numeric/Double.hs
+++ Numeric/Double.hs
@@ -1,5 +1,18 @@
+{-# language CPP #-}
+
 module Numeric.Double
-  ( Double
+  (
+    Double,
+#ifdef DEP_DOUBLE_CONVERSION
+    toExponential,
+    toFixed,
+    toPrecision,
+    toShortest,
+#endif
   ) where
 
 import GHC.Float
+#ifdef DEP_DOUBLE_CONVERSION
+import Data.Double.Conversion.Text
+  (toExponential, toFixed, toPrecision, toShortest)
+#endif
