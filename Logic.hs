0920accf93e38e8ed9a7e0b321c05277cbeb50a5
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-07-03 21:26:31 -0500

    Don't export Logic.observe

--- Logic.hs
+++ Logic.hs
@@ -1,5 +1,24 @@
 module Logic
-  ( module Control.Monad.Logic
+  ( -- ** Logic
+    Logic,
+    logic,
+    runLogic,
+    observeAll,
+    -- ** LogicT
+    LogicT(..),
+    runLogicT,
+    observeT,
+    observeManyT,
+    observeAllT,
+    -- ** MonadLogic
+    MonadLogic,
+    msplit,
+    interleave,
+    (>>-),
+    ifte,
+    once,
+    reflect,
+    lnot,
   ) where
 
 import Control.Monad.Logic
