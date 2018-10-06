ad7f52f0f9ada0f3730331481fffed9d38917ea2
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-10-05 23:21:46 -0500

    Few more test modules

--- planet-mitchell-test/src/Test.hs
+++ planet-mitchell-test/src/Test.hs
@@ -1,5 +1,19 @@
 module Test
-  ( module Hedgehog
+  ( -- * Running tests
+    defaultMain
+  , testGroup
+    -- * Unit tests
+  , testCase
+  , shouldBe
+  , shouldSatisfy
+  , shouldReturn
+    -- * Property tests
+  , testProperty
+  , module Hedgehog
   ) where
 
 import Hedgehog
+import Test.Hspec.Expectations
+import Test.Tasty
+import Test.Tasty.Hedgehog
+import Test.Tasty.HUnit
